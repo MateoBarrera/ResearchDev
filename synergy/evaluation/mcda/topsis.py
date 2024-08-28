7a1d4f846bf8e6066faa2456853e9b3b84f04ae2
MateoBarrera <materile1997@gmail.com>
2024-08-28 00:18:10 -0500

    feat: Update topsis algorithm implementation and indicator formulas

--- synergy/evaluation/mcda/topsis.py
+++ synergy/evaluation/mcda/topsis.py
@@ -224,8 +224,7 @@ def topsis(
         ideal_positive, ideal_negative = __topsis_ideal_solution(
             topsis_weighted_alternatives
         )
-        print(topsis_weighted_alternatives.shape)
-        print(ideal_negative.shape)
+
         positive_distance, negative_distance, similarity_index = __topsis_distance(
             topsis_weighted_alternatives, ideal_positive, ideal_negative
         )
